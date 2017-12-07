bdf4f820f3a41f11fd9b11ce5c98e9e7d3fe4ac9
Lucas Loureiro <lucas.loureiro@esx.com.br>
2017-12-07 10:15:31 -0200

    basic configuration from asp.net core identity and decopuling in another assembly

---
+++ MyFavThings/MyFavThings/Extensions/IdentityDataServicesExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MyFavThings.Database;
+using MyFavThings.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFavThings.Extensions
+{
+    public static class IdentityDataServicesExtension
+    {
+        public static IServiceCollection ConfigureIdentityProvider(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<MyFavThingsDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddIdentity<User, Role>()
+                    .AddEntityFrameworkStores<MyFavThingsDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+            });
+
+            return services;
+        }
+    }
+}
